819d5761e76d53b9e38bbe7d7532e84d18a5c800
AusteId <auste.idaite@gmail.com>
2025-01-16 23:18:35 +0200

    Add stream task

--- 13_praktiniai_stream/13_02.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ 13_praktiniai_stream/13_02.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -1,10 +1,15 @@
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import static java.nio.file.Path.of;
 
 public class ReadingFilesPerLine {
 
@@ -14,8 +19,9 @@ public class ReadingFilesPerLine {
 
     }
 
-    public static List<String> read(String file) {
-        return file.
+    public static List<String> read(String file) throws IOException {
+//        return Arrays.stream(file.split("\n")).collect(Collectors.toList());
+        return Files.lines(of(file)).collect(Collectors.toList());
     }
 
 }
--- 13_praktiniai_stream/13_03.BooksFromFile/src/main/java/BooksFromFile.java
+++ 13_praktiniai_stream/13_03.BooksFromFile/src/main/java/BooksFromFile.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -6,11 +7,20 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+import static java.nio.file.Path.of;
+
 public class BooksFromFile {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+
+    }
+
+    public static List<Book> readBooks(String file) throws IOException {
+        return Files.lines(of(file)).map(a -> a.split(","))
+                .map(a -> new Book(a[0], Integer.parseInt(a[1]), Integer.parseInt(a[2]),
+                        a[3])).collect(Collectors.toList());
 
     }
 
