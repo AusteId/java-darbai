ddc958e8fba4bb35562c44793b7685a9f0e5e7c2
AusteId <auste.idaite@gmail.com>
2025-01-20 08:59:12 +0400

    Add BimBam task

--- bimbam/src/main/java/lt/techin/BimBam.java
+++ bimbam/src/main/java/lt/techin/BimBam.java
@@ -25,7 +25,7 @@ public class BimBam {
     16
     */
     public static void bimBam(int number) {
-        for (int i = 0; i <= number; i++) {
+        for (int i = 1; i <= number; i++) {
             if(i % 3 == 0 && i % 5 == 0) {
                 System.out.println("BimBam");
             } else if (i % 3 == 0) {
