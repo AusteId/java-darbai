18f1a80f4f208f0a68ed1b1c79e37477d8a283be
AusteId <auste.idaite@gmail.com>
2025-01-27 12:20:53 +0400

    Add initial project setup for CurrencyBank task

---
+++ ATSISKAITYMAS/AusteIdaiteBank/src/main/java/lt/techin/BankTest.java
@@ -0,0 +1,18 @@
+package lt.techin;
+
+import lt.itakademija.exam.Bank;
+import lt.itakademija.exam.CurrencyConverter;
+import lt.itakademija.exam.CurrencyRatesProvider;
+import lt.itakademija.exam.test.BaseTest;
+
+public class BankTest extends BaseTest {
+    @Override
+    protected Bank createBank(CurrencyConverter currencyConverter) {
+        return new BankRuptcy();
+    }
+
+    @Override
+    protected CurrencyConverter createCurrencyConverter(CurrencyRatesProvider currencyRatesProvider) {
+        return new MyCurrencyConverter();
+    }
+}
---
+++ ATSISKAITYMAS/AusteIdaiteBank/src/main/java/lt/techin/MyCurrencyConverter.java
@@ -0,0 +1,13 @@
+package lt.techin;
+
+import lt.itakademija.exam.Currency;
+import lt.itakademija.exam.CurrencyConverter;
+import lt.itakademija.exam.Money;
+
+public class MyCurrencyConverter implements CurrencyConverter {
+    
+    @Override
+    public Money convert(Currency currency, Currency currency1, Money money) {
+        return null;
+    }
+}
